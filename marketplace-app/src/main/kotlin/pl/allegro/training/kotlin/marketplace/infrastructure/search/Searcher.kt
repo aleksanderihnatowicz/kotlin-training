e765d3bc89c1a9a26d10175368068d09dcac0965
Konrad Kaminski <konrad.kaminski@allegrogroup.com>
2017-10-16 11:44:49 +0200

    Searcher improvement.

--- marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/infrastructure/search/Searcher.kt
+++ marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/infrastructure/search/Searcher.kt
@@ -3,18 +3,26 @@ package pl.allegro.training.kotlin.marketplace.infrastructure.search
 import pl.allegro.training.kotlin.marketplace.infrastructure.search.index.Index
 import pl.allegro.training.kotlin.marketplace.infrastructure.search.parser.QueryParser
 
+typealias  Scorer = (docId: DocumentId) -> Double
 
-class Searcher(private val index: Index) {
-    private val parser = QueryParser()
+typealias ScorerFactory = (optionalDocs: Set<DocumentId>) -> Scorer
+
+class Searcher(
+    private val index: Index,
+    private val scorerFactory: ScorerFactory = boostingScorerFactory,
+    private val parser: QueryParser = QueryParser()
+) {
 
     fun search(queryText: String): List<DocumentId> {
-        if (queryText.isEmpty()) throw EmptyQueryException()
         val query = parser.parse(queryText)
+        if (query.isEmpty()) throw EmptyQueryException()
         val validDocs = index.getDocumentsWithTokens(query.filter { phrase -> phrase.required }.map { it.value })
         val invalidDocs = index.getDocumentsWithTokens(query.filter { phrase -> phrase.forbidden }.map { it.value })
         val optionalDocs = index.getDocumentsWithTokens(query.filter { phrase -> phrase.optional }.map { it.value })
+        val scorer = scorerFactory.invoke(optionalDocs)
         // operation on sets with operators
-        return (validDocs - invalidDocs).sortedWith(BoostingComparator(optionalDocs).reversed())
+        // scorer as a high-order function
+        return (validDocs - invalidDocs).sortedBy(scorer)
     }
 
     private fun Index.getDocumentsWithTokens(tokens: List<String>): Set<DocumentId> =
@@ -23,7 +31,8 @@ class Searcher(private val index: Index) {
 
 class EmptyQueryException : RuntimeException()
 
-class BoostingComparator(private val boostedIds: Set<DocumentId>) : Comparator<DocumentId> {
-    override fun compare(aDocId: DocumentId?, bDocId: DocumentId?): Int =
-            boostedIds.contains(aDocId).compareTo(boostedIds.contains(bDocId))
+private val boostingScorerFactory: ScorerFactory = { optionalDocs ->
+    { docId -> (docId in optionalDocs).asDouble() }
 }
+
+private fun Boolean.asDouble(): Double = if (this) 1.0 else 0.0
