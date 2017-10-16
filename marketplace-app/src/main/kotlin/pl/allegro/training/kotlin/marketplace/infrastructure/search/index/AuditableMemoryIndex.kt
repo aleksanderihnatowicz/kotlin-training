0aa87802df1b5dab56b77be6b1885a9e9af40e1c
Konrad Kaminski <konrad.kaminski@allegrogroup.com>
2017-10-16 12:02:26 +0200

    delegation with by example.

---
+++ marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/infrastructure/search/index/AuditableMemoryIndex.kt
@@ -0,0 +1,15 @@
+package pl.allegro.training.kotlin.marketplace.infrastructure.search.index
+
+import pl.allegro.training.kotlin.marketplace.infrastructure.search.DocumentId
+import pl.allegro.training.kotlin.marketplace.infrastructure.logger
+
+class AuditableMemoryIndex(private val delegate: MemoryIndex): Index by delegate {
+    override fun addTokenOccurrence(occurrence: Pair<String, DocumentId>) {
+        logger.debug("Adding token {} with document {}", occurrence.first, occurrence.second)
+        delegate.addTokenOccurrence(occurrence)
+    }
+
+    companion object {
+        private val logger by logger()
+    }
+}
