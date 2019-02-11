fb12a5827b31c2bfbd3c210642d6d8f997db9596
Piotr Procner <piotr.procner@allegro.pl>
2019-02-11 18:49:54 +0100

    Migrates build scripts to Kotlin DSL.

---
+++ build.gradle.kts
@@ -0,0 +1,2 @@
+group = "pl.allegro.training.kotlin"
+version = "1.0.0"
---
+++ marketplace-app/build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    groovy
+    kotlin("jvm") version "1.3.21"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.21"
+    id("org.springframework.boot") version "2.1.2.RELEASE"
+    id("io.spring.dependency-management") version "1.0.6.RELEASE"
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(project(":marketplace-utils"))
+
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.3")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.spockframework:spock-core:1.2-groovy-2.5")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions.jvmTarget = "1.8"
+
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions.jvmTarget = "1.8"
