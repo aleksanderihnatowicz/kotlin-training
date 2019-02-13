9917607db72dbe54ddc8d45bf4fe149a474cc582
Piotr Procner <piotrek.procner@gmail.com>
2019-02-13 09:02:24 +0100

    Moves status endpoint to separate package.

--- marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/adapter/rest/status/StatusController.kt
+++ marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/adapter/rest/status/StatusController.kt
@@ -1,4 +1,4 @@
-package pl.allegro.training.kotlin.marketplace.adapter.rest
+package pl.allegro.training.kotlin.marketplace.adapter.rest.status
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
