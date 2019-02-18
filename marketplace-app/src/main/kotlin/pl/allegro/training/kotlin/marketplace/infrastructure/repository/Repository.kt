387d6fbec2269972e713ac4273454326647d4431
Piotr Procner <piotrek.procner@gmail.com>
2019-02-18 09:54:38 +0100

    Removes operator function from Repository interface.

--- marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/infrastructure/repository/Repository.kt
+++ marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/infrastructure/repository/Repository.kt
@@ -4,5 +4,5 @@ interface Repository<Entity : Identifiable<Id>, Id> {
     fun save(entity: Entity)
     fun findById(id: Id): Entity?
     fun findAll(): List<Entity>
-    operator fun contains(id: Id): Boolean
+    fun contains(id: Id): Boolean
 }
