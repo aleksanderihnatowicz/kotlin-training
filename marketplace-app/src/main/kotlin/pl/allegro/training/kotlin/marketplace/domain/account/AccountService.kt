afb02547a4a272e716124c915733537933a5857c
Piotr Procner <piotrek.procner@gmail.com>
2019-02-18 09:31:10 +0100

    Renames accountRepository to repository in AccountService.

--- marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/domain/account/AccountService.kt
+++ marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/domain/account/AccountService.kt
@@ -4,18 +4,18 @@ import pl.allegro.training.kotlin.marketplace.infrastructure.id.IdGenerator
 import pl.allegro.training.kotlin.marketplace.infrastructure.logger
 
 class AccountService(
-    private val accountRepository: AccountRepository,
+    private val repository: AccountRepository,
     private val idGenerator: IdGenerator
 ) {
     fun addAccount(account: Account): Account {
         val accountWithId = account.copy(id = account.id ?: idGenerator.getNextId())
         logger.debug("Adding account with id = {}", accountWithId)
-        accountRepository.save(accountWithId)
+        repository.save(accountWithId)
         return accountWithId
     }
 
     fun getAccountsByStatus(status: AccountStatus?): List<Account> =
-        accountRepository.findAll()
+        repository.findAll()
             .filter { status == it.status || status == null }
 
     companion object {
