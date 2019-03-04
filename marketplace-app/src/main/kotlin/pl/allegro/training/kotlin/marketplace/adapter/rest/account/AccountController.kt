d42d98e75ea7bfc716ac75855b245d91c26536ba
Piotr Procner <piotrek.procner@gmail.com>
2019-03-04 17:02:12 +0100

    Replaces ResponseEntity wrapper with @ResponseStatus annotations in AccountController and OfferController.

--- marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/adapter/rest/account/AccountController.kt
+++ marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/adapter/rest/account/AccountController.kt
@@ -1,13 +1,13 @@
 package pl.allegro.training.kotlin.marketplace.adapter.rest.account
 
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import pl.allegro.training.kotlin.marketplace.domain.account.AccountService
 import pl.allegro.training.kotlin.marketplace.domain.account.AccountStatus
@@ -18,9 +18,10 @@ import pl.allegro.training.kotlin.util.hash.SecureHashAlgorithmUtils as SHA
 class AccountController(private val accountService: AccountService) {
 
     @PostMapping
-    fun addAccount(@RequestBody @Validated creationRequest: AccountCreationRequest): ResponseEntity<AccountResponse> {
+    @ResponseStatus(HttpStatus.CREATED)
+    fun addAccount(@RequestBody @Validated creationRequest: AccountCreationRequest): AccountResponse {
         val account = accountService.addAccount(creationRequest.asAccount())
-        return ResponseEntity(account.asAccountResponse(), HttpStatus.CREATED)
+        return account.asAccountResponse()
     }
 
     @GetMapping
--- marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/adapter/rest/offer/OfferController.kt
+++ marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/adapter/rest/offer/OfferController.kt
@@ -2,12 +2,12 @@ package pl.allegro.training.kotlin.marketplace.adapter.rest.offer
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import pl.allegro.training.kotlin.marketplace.domain.offer.OfferService
 
@@ -18,13 +18,14 @@ class OfferController {
     private lateinit var offerService: OfferService
 
     @PostMapping("/accounts/{accountId}/offers")
+    @ResponseStatus(HttpStatus.CREATED)
     fun addOffer(
         @PathVariable accountId: String,
         @RequestBody creationRequest: OfferCreationRequest
-    ): ResponseEntity<OfferResponse> {
-        val offer = offerService.addOffer(creationRequest.asOffer(accountId))
-        return ResponseEntity(offer.asOfferResponse(), HttpStatus.CREATED)
-    }
+    ): OfferResponse =
+        offerService
+            .addOffer(creationRequest.asOffer(accountId))
+            .asOfferResponse()
 
     @GetMapping("/offers")
     fun findOffers(@RequestParam(defaultValue = "") query: String): OfferListResponse =
