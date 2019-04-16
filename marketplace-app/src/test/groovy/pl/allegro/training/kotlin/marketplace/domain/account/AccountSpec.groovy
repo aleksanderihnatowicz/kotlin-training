03194b37852c111ef2de923f7bc5598c881d6d9b
Piotr Procner <piotrek.procner@gmail.com>
2019-04-16 21:52:28 +0200

    Removes AccountTestBuilder to show example of use of @JvmOverloads.

--- marketplace-app/src/test/groovy/pl/allegro/training/kotlin/marketplace/domain/account/AccountSpec.groovy
+++ marketplace-app/src/test/groovy/pl/allegro/training/kotlin/marketplace/domain/account/AccountSpec.groovy
@@ -2,8 +2,6 @@ package pl.allegro.training.kotlin.marketplace.domain.account
 /*
 import spock.lang.Specification
 
-import static pl.allegro.training.kotlin.marketplace.domain.account.AccountTestBuilder.account
-
 class AccountSpec extends Specification {
     def "should throw on creation when email is invalid"() {
         given:
@@ -11,17 +9,6 @@ class AccountSpec extends Specification {
 
         when:
         new Account("1", "john.doe", "A4B23FE1", invalidEmail, null, AccountStatus.ACTIVE, 0)
-
-        then:
-        thrown InvalidAccountException
-    }
-
-    def "with test builder"() {
-        given:
-        def invalidEmail = "gmail.com"
-
-        when:
-        account(email: invalidEmail)
 
         then:
         thrown InvalidAccountException
