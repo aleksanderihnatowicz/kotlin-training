652cf1f5f6022d128aaa7b546b309b5e629ddc37
Piotr Procner <piotrek.procner@gmail.com>
2019-04-19 15:30:55 +0200

    Removes Validation.kt and adds notes.txt file with email validation regex.

--- marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/infrastructure/Validation.kt
+++
@@ -1,5 +0,0 @@
-package pl.allegro.training.kotlin.marketplace.infrastructure
-
-private const val EMAIL_REGEX = "[a-zA-Z.]+@[a-zA-Z.]+"
-
-fun isValidEmail(email: String): Boolean = email matches Regex(EMAIL_REGEX)
