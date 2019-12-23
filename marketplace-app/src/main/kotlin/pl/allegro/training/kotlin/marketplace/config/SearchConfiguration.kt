5165a604ef7128d7b6b02ece4e3f11dd6cbab6bf
Aleksander Ihnatowicz <aleksander.ihnatowicz@allegro.pl>
2019-12-23 13:46:33 +0100

    Bump spring boot to 2.2.2

--- marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/config/SearchConfiguration.kt
+++ marketplace-app/src/main/kotlin/pl/allegro/training/kotlin/marketplace/config/SearchConfiguration.kt
@@ -1,8 +1,10 @@
 package pl.allegro.training.kotlin.marketplace.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 
+@ConstructorBinding
 @ConfigurationProperties(prefix = "search")
-class SearchConfiguration {
-    var allowEmptyQuery: Boolean = false
-}
+data class SearchConfiguration(
+    val allowEmptyQuery: Boolean
+)
