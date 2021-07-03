7eb12d26b76858b88705a3abf263ee37b38d9f97
Piotr Procner <piotr.procner@allegro.pl>
2021-07-03 13:15:27 +0200

    Upgrades all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,10 @@
 group = "pl.allegro.training.kotlin"
 version = "1.0.0"
+
+plugins {
+    kotlin("jvm") version "1.5.20"
+}
+
+repositories {
+    mavenCentral()
+}
--- marketplace-app/build.gradle.kts
+++ marketplace-app/build.gradle.kts
@@ -3,14 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     application
     groovy
-    kotlin("jvm") version "1.4.0"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.4.0"
-    id("org.springframework.boot") version "2.2.2.RELEASE"
-    id("io.spring.dependency-management") version "1.0.6.RELEASE"
+    kotlin("jvm")
+    id("org.jetbrains.kotlin.plugin.spring") version "1.5.20"
+    id("org.springframework.boot") version "2.5.2"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
 }
 
 repositories {
-    jcenter()
+    mavenCentral()
 }
 
 dependencies {
@@ -20,20 +20,20 @@ dependencies {
     implementation(kotlin("reflect"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.3")
+    implementation("javax.validation:validation-api:2.0.1.Final")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.spockframework:spock-core:1.3-groovy-2.5")
+    testImplementation("org.spockframework:spock-core:2.0-groovy-3.0")
 }
 
 application {
-    mainClassName = "pl.allegro.training.kotlin.marketplace.MarketplaceApplicationKt"
+    mainClass.set("pl.allegro.training.kotlin.marketplace.MarketplaceApplicationKt")
 }
 
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
-    freeCompilerArgs = listOf("-XXLanguage:+InlineClasses")
 }
 
 val compileTestKotlin: KotlinCompile by tasks
